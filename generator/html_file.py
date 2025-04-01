cf0c4e9b1f799aa95116145dfe2474047664bac2
Octavio <116118454+Octavio-Lima@users.noreply.github.com>
2025-04-01 15:44:08 -0300

    Save result html file

--- generator/html_file.py
+++ generator/html_file.py
@@ -5,3 +5,8 @@ def get_filename(user_variable: dict[str, Any]):
     return user_variable.get(
         "filename", user_variable.get("user_name", "undefined_filename")
     )
+
+
+def save(filename: str, content: str):
+    with open(f"results/{filename}.html", "w", encoding="utf-8") as update_file:
+        update_file.write(content)
